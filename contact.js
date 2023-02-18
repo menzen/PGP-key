0f13d37778abccd6336003573bc9e0b0af751741
menzen <menzen@users.noreply.github.com>
2023-02-18 13:05:13 +0100

    Update and rename index.js to contact.js

---
+++ contact.js
@@ -0,0 +1,1 @@
+console.log(JSON.parse(Buffer.from('eyJuYW1lIjoiTWljaGFlbCBNZW56ZWwiLCJwcm9mZXNzaW9uIjoic29mdHdhcmUgZW5naW5lZXIiLCJlbWFpbCI6Im1pY2hhZWxfbWVuemVsQGljbG91ZC5jb20ifQ==', 'base64').toString('binary')))
